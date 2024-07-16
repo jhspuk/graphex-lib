a3c73b505dc79b4612e2d88dec9f9a360975ede4
jhspuk <j.hugh.sp@gmail.com>
2024-07-16 13:00:13 +0100

    import code broadly completed

--- src/library-test1/graphex.cpp
+++ src/library-test1/graphex.cpp
@@ -276,95 +276,18 @@ namespace graphex{
 					
 					//cout<<tr->label<<endl;
 				} else if(flag6){
-					for(auto&i : f_tr_map){
-						for(auto&k : i.second->inputs){
-							cout<<k->new_label<<endl;
-						}
-					}
+					D(cout<<"File read: "<<path<<endl;)
+					break;
 				}
 				
+				
+				
 			}
-			
-			/*
-			struct f_pl_sub{
-				std::string sub_specific;
-				std::vector<int> whitelist;
-			};
-			
-			//.lpn lists all transitions in as "dummies"
-			std::unordered_set<std::string> f_tran_list;
-			//grab all place labels
-			std::unordered_set<std::string> f_place_list;
-			//vector of substituions
-			std::unordered_map<std::string, f_pl_sub> f_sub_list;
-			
-			//*******file iteration -- loop over file to first find the
-			//labels of places and transitions (needed because special 
-			//names will link different place groups). Then, loop a second
-			//time, searching for labels in created place groups. If those
-			//places do not exist, then create them. Then link transitions
-			//by again searching file for labels.
-			std::string line; bool flag = 0;
-			//***first pass of file to find transitions and places
-			while(std::getline(file,line)){
-				std::istringstream iss(line);
-				std::string token;
-				iss >> token;
-				if(flag == 0){
-					if(token == ".dummy"){
-						while (iss >> token){
-							std::cout<<"found transition: "<<token<<std::endl;
-							f_tran_list.insert(token);
-						}
-					} else if(token == ".graph"){
-						flag=1;
-					}
-				} else {
-					if(token.rfind(".",0)==0){flag = 0;}
-					else {
-						if(f_tran_list.find(token) == f_tran_list.end()){
-							
-							f_place_list.insert(token);
-							
-							int s_start = token.find("x_"); int s_end = 2;
-							if(s_start==0){
-								//gained
-								s_start = s_end;
-								s_end = token.find("_", s_start);
-								std::string label_set_name = token.substr(s_start,s_end-s_start);
-								//-gained
-								std::cout<<"found special place: "<<token.substr(s_start,s_end-s_start)<<std::endl;
-								std::cout<<s_start<<" + "<<s_end<<std::endl;
-								for(auto&i : patterns){
-									if(label_set_name==i.name){
-										f_sub_list.insert({token,f_pl_sub{token.replace(s_start,s_end-s_start,i.sub),i.whitelist}});
-										std::cout<<"Made substitution: "<<token.replace(s_start,s_end-s_start+1,i.sub)<<std::endl;
-									}
-								}
-							}
-						}
-					}
+			for(auto&i : f_tr_map){
+				for(auto&k : i.second->inputs){
+					cout<<k->new_label<<endl;
 				}
 			}
-			
-			for(auto &k:f_sub_list){
-				auto vi = k.second.whitelist;
-				for(auto &i:vi){
-					std::cout<<"Whitelists: "<<i<<std::endl;
-				}
-			}
-			
-			file.seekg(0);
-			
-			std::cout<<"Stating: "<<std::endl;
-			for(auto& i:f_tran_list){
-				std::cout<<i<<std::endl;
-			}
-			for(auto& i:f_place_list){
-				std::cout<<i<<std::endl;
-			}
-			*/
-			
 		}
 		
 		//cpp nonsense -- workaround for cpp definition not in .h

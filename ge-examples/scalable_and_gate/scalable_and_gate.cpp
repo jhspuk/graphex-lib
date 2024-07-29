d7f971ee6a93b39a9e902e41482bbe3fce82d9c9
jhspuk <j.hugh.sp@gmail.com>
2024-07-29 14:52:00 +0100

    worked example

--- ge-examples/scalable_and_gate/scalable_and_gate.cpp
+++ ge-examples/scalable_and_gate/scalable_and_gate.cpp
@@ -36,10 +36,30 @@ int r_fan_in(T_graph* pn, vector<int>& leafnodes, int depth){
 	return 1;
 }
 
-
-
-int r_fan_out(T_graph*){
+//recursive builder for fan out, returns indexes of leafnodes
+int r_fan_out(T_graph* pn, vector<int>& leafnodes, int depth, int previous){
 	
+	int temp_tr_index; vector<int> temp_places_match;
+	pn->find_pg(previous, temp_places_match);
+	
+	if(depth == 0){
+		leafnodes.push_back(previous);
+		return 1;
+	} else {
+		for(int i=0; i<4; i++){
+			temp_tr_index = pn->add("pn/g_fan_out_4.lpn",std::vector<pattern>{{"in1","out"+to_string(i+1),temp_places_match}});
+			r_fan_in(pn, leafnodes, depth - 1, temp_tr_index);
+		}
+		return 1;
+	}
+	return 1;
+}
+
+int r_fan_out(T_graph* pn, vector<int>& leafnodes, int depth){
+	
+	leafnodes.push_back(pn->add("pn/g_fan_out_4.lpn",vector<pattern>{}));
+	r_fan_in(pn, leafnodes, depth, leafnodes[0]);
+	return 1;
 }
 
 int main(){
@@ -47,9 +67,20 @@ int main(){
 	Graph<PL_frame<int,int>,TR_index_frame<int,int>> large_and_gate;
 	int temp_tr_index; vector<int> temp_places_match;
 	
-	vector<int> leafnodes;
+	vector<int> leafnodes_in;
+	vector<int> leafnodes_out;
 	//leafnodes.push_back(large_and_gate.add("pn/g_fan_in.lpn",vector<pattern>{}));
-	r_fan_in(&large_and_gate, leafnodes, 4);
+	r_fan_in(&large_and_gate, leafnodes_in, 2);
+	
+	large_and_gate.find_pg(leafnodes_in[0], temp_places_match);
+	
+	uint8_t input = 0;
+	
+	large_and_gate.attach({"x_out1_1","",temp_places_match});
+	
+	large_and_gate.get(0, input);
+	cout<<"here is the input: "<<(int)input<<endl;
+	//r_fan_out(&large_and_gate, leafnodes_out, 4);
 	
 	/*
 	for(auto& i:leafnodes){

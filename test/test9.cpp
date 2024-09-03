64dea6e55c5ff60f0a1b7bb4b827026bea02d271
jhspuk <j.hugh.sp@gmail.com>
2024-09-03 11:13:37 +0100

    finished behaviours of graph test

--- test/test9.cpp
+++ test/test9.cpp
@@ -1,3 +1,5 @@
+//demo of new graph behaviours...
+
 #include <iostream>
 #include <variant>
 #include <vector>
@@ -33,7 +35,7 @@ struct gn_func : public gn_base{
 	void (*func)(gn_base* ctx);
 };
 
-void add(gn_base* ctx){
+void f_add(gn_base* ctx){
 	
 	using T_data = gn_data<int>;
 
@@ -54,6 +56,35 @@ void add(gn_base* ctx){
 	return;
 }
 
+void f_join(gn_base* a, gn_base* b, uint8_t mode){
+	switch(mode){
+		//forwards join, given a, b is output
+		case 0:
+			b->con.push_back({a,e_sl::input});
+			a->con.push_back({b,e_sl::output});
+			break;
+		//backwards join, given a, b is input
+		case 1:
+			b->con.push_back({a,e_sl::output});
+			a->con.push_back({b,e_sl::input});
+			break;
+		//symmetric join, input
+		case 2:
+			b->con.push_back({a,e_sl::input});
+			a->con.push_back({b,e_sl::input});
+			break;
+		//symmetric join, output
+		case 3:
+			b->con.push_back({a,e_sl::output});
+			a->con.push_back({b,e_sl::output});
+			break;
+		default:
+			break;
+	}
+
+	return;
+}
+
 struct area_gn{
 	std::vector<gn_base*> gn_data_reg;
 	std::vector<gn_base*> gn_func_reg;
@@ -71,14 +102,50 @@ int main(){
 
 	area_gn m_reg;
 
-	m_reg.gn_data_reg.push_back(new gn_data<int>);
-	m_reg.gn_data_reg.push_back(new gn_data<int>);
-	m_reg.gn_data_reg.push_back(new gn_data<int>);
-	m_reg.gn_data_reg.push_back(new gn_data<int>);
+	int counter = 5;
+	gn_data<int>* z_a;
 
-	m_reg.gn_func_reg.push_back(new gn_func);
+	for(int i = 0; i < 10; i++){	
+		z_a = new gn_data<int>;
+		z_a->data = counter++;
+		m_reg.gn_data_reg.push_back(z_a);
+	}
 
+	gn_func* z_b;
+	z_b = new gn_func;
+	z_b->func = f_add;
+	m_reg.gn_func_reg.push_back(z_b);
+
+	for(auto& i : m_reg.gn_data_reg){
+		f_join(z_b, i, 1);
+	}	
+
+	z_a = new gn_data<int>;
+	z_a->data = 0;
+	m_reg.gn_data_reg.push_back(z_a);
+
+	f_join(z_b, z_a, 0);
+	cout<<"Data nodes..."<<endl;
+
+	z_b->func(z_b);
+
+	for(auto& i : m_reg.gn_data_reg){
+		cout<<"Node: "<<i<<" Data: "<<((gn_data<int>*)i)->data<<endl;
+		for(auto& k : i->con){
+			cout<<k.first<<" + "<<(int)(k.second)<<endl;
+		}
+	}
 	
+	cout<<"Function nodes..."<<endl;
+
+	for(auto& i : m_reg.gn_func_reg){
+		cout<<"Node: "<<i<<endl;
+		for(auto& k : i->con){
+			cout<<k.first<<" + "<<(int)(k.second)<<endl;
+		}
+	}
+	
+
 
 	return 1;
 }
